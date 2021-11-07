18e837b3f17ee32177b2fa2b9caaa06c586d0017
CarusCha <caruscha.hk@gmail.com>
2021-11-08 00:20:12 +0800

    edit
    added something
    edit css

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,37 +1,25 @@
 const navbarHTML = 
-`<nav class="navbar navbar-expand-lg navbar-light bg-light">
+`<nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="nav_bar_all">
 <div class="container-fluid">
-  <a class="navbar-brand" href="#">Navbar</a>
+<label id="nav_title"><p>Passionable developer</p>, <p>Carus Cha's website</p></label>
+  <a id="nav_title" class="navbar-brand block-with-text" href="index.html"></a>
   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+    </ul>
+      <ul class="navbar-nav mb-2 mb-lg-0">
       <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a id="family" class="nav-link" href="family.html">Family</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          Dropdown
-        </a>
-        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <li><a class="dropdown-item" href="#">Action</a></li>
-          <li><a class="dropdown-item" href="#">Another action</a></li>
-          <li><hr class="dropdown-divider"></li>
-          <li><a class="dropdown-item" href="#">Something else here</a></li>
-        </ul>
+        <a id="interest" class="nav-link" href="interest.html">Interest</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link disabled">Disabled</a>
+        <a id="skill" class="nav-link" href="skill.html">Skill</a>
       </li>
     </ul>
-    <form class="d-flex">
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success" type="submit">Search</button>
-    </form>
   </div>
 </div>
 </nav>
@@ -41,5 +29,19 @@ const navbarHTML =
 
 document.addEventListener("DOMContentLoaded", function() {
     document.querySelector("#nav_bar").innerHTML = navbarHTML;
+    document.querySelectorAll('a[class="nav');
+    console.log(window.location.pathname);
+    switch(window.location.pathname) {
+    case "/family.html":
+      document.querySelector('a[id="family"]').className += " active";
+      // document.querySelector('a[id="family"]').classList.add("active");
+      // navbar.classList.remove("sticky");
+      break;
+    case "/interest.html":
+      document.querySelector('a[id="interest"]').className += " active";
+      break;
+    case "/skill.html":
+      document.querySelector('a[id="skill"]').className += " active";
+    }
 });
 
